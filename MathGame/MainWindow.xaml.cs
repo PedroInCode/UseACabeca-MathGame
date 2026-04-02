994fef519b945e4ab3ece75055f128e99b101672
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-04-02 16:50:44 -0300

    Adicionar arquivos de projeto.

---
+++ MathGame/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathGame
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> AnimalEmoji = new List<string>()
+            {
+                "🦑", "🦑",
+                "🐡", "🐡",
+                "🐘", "🐘",
+                "🐳", "🐳",
+                "🐫", "🐫",
+                "🦖", "🦖",
+                "🦘", "🦘",
+                "🦔", "🦔",
+
+            };
+            Random random = new Random();
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(AnimalEmoji.Count);
+                string nextEmoji = AnimalEmoji[index];
+                textBlock.Text = nextEmoji;
+                AnimalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
